77c9026566949bc91ab601b19dbae806df76c3ed
Gabriel dos Reis <dosreisg@dupage.edu>
2024-03-08 10:55:01 -0600

    Finished tic_tac_toe program

--- Week 2/tic_tac_toe.py
+++ Week 2/tic_tac_toe.py
@@ -15,65 +15,105 @@ def main():
 
 def create_board():
     board = np.arange(1, 10).reshape(3, 3)
+    board = board.astype(str)
     return board
 
-def place_marker(coord_x, coord_y, player):
+def place_marker(board, coord_x, coord_y, player):
     '''
     This function places a marker on the board
         X or O  
     '''
-    board = create_board()
-    board[coord_x][coord_y] = player
+    board[coord_x, coord_y] = player
+    return board
 
-def print_board():
-    pass
+# A function to print the 3x3 2D list and make it look like a board
+def print_board(board):
+    for item in board:
+        print(item)
 
-def check_winner():
-    pass
+def check_winner(board, player):
+    '''
+    Check if the specified player has won the game.
+    
+    Parameters:
+    - board: A 3x3 NumPy array representing the current state of the game board.
+    - player: The player's marker ('X' or 'O') to check for a win.
+    
+    Returns:
+    - True if the specified player has won the game, False otherwise.
+    '''
+    # Define the array for the specified player's markers
+    player_arr = np.full((3, 3), player)
 
-def check_draw():
-    pass
+    if np.any(np.all(board == player_arr, axis=1)):
+        return True
+
+    # Columns
+    if np.any(np.all(board == player_arr, axis=0)):
+        return True
+
+    # Diagonals
+    if np.all(np.diag(board) == player) or np.all(np.diag(np.fliplr(board)) == player):
+        return True
+
+    return False
+
+def check_draw(board):
+    for i in range(1,10):
+        if np.any(board == str(i)):
+            return False
+    return True
 
 def play_game():
     '''
     This function will start the game and call all the other functions
     
     '''
+
     player1 = input("Player 1, do you want to be X or O? ").upper()
     if player1 == "X":
         player2 = "O"
     else:
         player2 = "X"
     print(f"Player 1 is {player1} and Player 2 is {player2}")
+
     board = create_board()
-    full = False
-    
-    while board is not full:
+    print(board)
+
+    while True:
         # Player 1
-        coord_x = int(input("Player 1, enter the x coordinate: "))
-        coord_y = int(input("Player 1, Enter the y coordinate: "))
-        place_marker(coord_x, coord_y, player1)
-        print_board()
-        if check_winner():
+        coord_x = int(input("Player 1, enter the x coordinate: ")) - 1
+        coord_y = int(input("Player 1, Enter the y coordinate: ")) - 1
+        board = place_marker(board, coord_x, coord_y, player1)
+        print(board)
+
+        if check_winner(board, player1):
             print("We have a winner!")
-            break
-        if check_draw():
+            replay()
+
+        if check_draw(board): 
             print("It's a draw!")
-            break
-        
+            replay()
+                    
         # Player 2
-        coord_x2 = int(input("Player 2, enter the x coordinate: "))
-        coord_y2 = int(input("Player 2, Enter the y coordinate: "))
-        place_marker(coord_x2, coord_y2, player2)
-        print_board()
-        if check_winner():
+        coord_x2 = int(input("Player 2, enter the x coordinate: ")) - 1
+        coord_y2 = int(input("Player 2, Enter the y coordinate: ")) - 1
+        board = place_marker(board, coord_x2, coord_y2, player2)
+        print(board)
+        if check_winner(board, player2):
             print("We have a winner!")
-            break
-        if check_draw():
+            replay()
+
+        if check_draw(board):
             print("It's a draw!")
-            break
+            replay()
 
 def replay():
-    pass
+    
+    replay = input("Would you like to play again? (y/n): ").lower()
+    if replay == "y":
+        play_game()
+    else:
+        print("It was nice playing with you. See you next time.")
 
 main()
